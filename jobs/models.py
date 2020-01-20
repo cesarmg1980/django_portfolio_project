caf52dcee1e86b51fa1e5e1d37a7016d11343983
cesar giulietti <cgiulietti@rccl.com>
2020-01-20 01:14:32 -0300

    changed from sqlite to postgres

--- jobs/models.py
+++ jobs/models.py
@@ -4,3 +4,6 @@ from django.db import models
 class Jobs(models.Model):
     image = models.ImageField(upload_to='images/')
     summary = models.CharField(max_length=200)
+
+    class Meta():
+        verbose_name_plural = 'Jobs'
