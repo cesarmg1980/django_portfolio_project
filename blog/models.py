c579c81f2bc45f8f5b084ebbf14add7754c5452b
Cesar <cesargiulietti@gmail.com>
2020-01-20 11:46:37 -0300

    added the blog app

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+class Blog(models.Model):
+    title = models.CharField(max_length=255)
+    published_date = models.DateTimeField(default=timezone.now)
+    body = models.TextField()
+    image = models.ImageField(upload_to='blog/images/', blank=True)
+
+    class Meta():
+        verbose_name_plural = 'Blog'
+    
+    def __str__(self):
+        return self.title + " " + str(self.published_date)
