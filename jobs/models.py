59c61f5581312fc9f10bf3c2769614e7f5e83f97
Cesar <cesargiulietti@gmail.com>
2020-01-21 01:06:05 -0300

    home page almost finished, linked with the db

--- jobs/models.py
+++ jobs/models.py
@@ -2,8 +2,12 @@ from django.db import models
 
 # Create your models here.
 class Jobs(models.Model):
+    short_name = models.CharField(max_length=50, default='no_project_name')
     image = models.ImageField(upload_to='images/')
     summary = models.CharField(max_length=200)
 
     class Meta():
         verbose_name_plural = 'Jobs'
+    
+    def __str__(self):
+        return self.short_name
--- jobs/views.py
+++ jobs/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from jobs.models import Jobs
 
 # Create your views here.
+
+def home(request):
+    jobs = Jobs.objects
+    return render(request, "jobs/home.html", {'jobs':jobs})
