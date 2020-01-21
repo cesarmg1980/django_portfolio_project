fcd4c951d1a8de9d35feb7e1ba06a65fc3553c44
Cesar <cesargiulietti@gmail.com>
2020-01-21 17:24:23 -0300

    Added Blog Page

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from blog.models import Blog
 
 # Create your views here.
+
+def blog_homepage(request):
+    blog_posts = Blog.objects
+    return render(request, "blog/home.html", {'blog_posts':blog_posts})
